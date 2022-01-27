9cf9606f60bb39207eb7768a62c2529ed63b2aff
Kiril Komitski <k.komitski94@gmail.com>
2022-01-27 17:12:49 +0000

    Create and Read functionaility

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { db } from './firebase-config';
+import { React, useEffect, useState } from 'react';
+import { collection, doc, getDocs, addDoc } from 'firebase/firestore';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [newName, setNewName] = useState('');
+	const [newAge, setNewAge] = useState(0);
+
+	// populate with content
+	const [users, setUsers] = useState([]);
+
+	// collection(database, collection)
+	const usersRef = collection(db, 'users');
+
+	// Create
+	const createUser = async () => {
+		await addDoc(usersRef, { name: newName, age: newAge });
+	};
+
+	useEffect(() => {
+		const getUsers = async () => {
+			const data = await getDocs(usersRef);
+			setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+		};
+		getUsers();
+	}, []);
+
+	return (
+		<>
+			<div className='App'>
+				<input
+					type='text'
+					placeholder='name'
+					onChange={(e) => {
+						setNewName(e.target.value);
+					}}
+				/>
+				<input
+					type='number'
+					placeholder='age'
+					onChange={(e) => {
+						setNewAge(e.target.value);
+					}}
+				/>
+				<button onClick={createUser}>CREATE USER</button>
+				{users.map((user) => {
+					return (
+						<div>
+							<h1>name:{user.name}</h1>
+							<h1>name:{user.age}</h1>
+						</div>
+					);
+				})}
+			</div>
+		</>
+	);
 }
 
 export default App;
